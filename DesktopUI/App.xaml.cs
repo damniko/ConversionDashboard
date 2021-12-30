c0c010402870eade8e67a677df4e7296387a6957
Nikolaj Dam <ndam20@student.aau.dk>
2021-12-30 22:13:16 +0100

    Created initial MVVM setup with dependency injection

---
+++ DesktopUI/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using DesktopUI.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DesktopUI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            Services = ConfigureServices();
+        }
+
+        /// <summary>
+        /// Gets the current <see cref="App"/> instance in use
+        /// </summary>
+        public new static App Current => (App)Application.Current;
+
+        /// <summary>
+        /// Gets the <see cref="IServiceProvider"/> instance to resolve application services.
+        /// </summary>
+        public IServiceProvider Services { get; }
+
+        /// <summary>
+        /// Configures the services for the application.
+        /// </summary>
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            services
+                .AddTransient<ShellViewModel>();
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
---
+++ DesktopUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopUI.ViewModels
+{
+    public class ShellViewModel
+    {
+        public string HelloWorld { get; set; } = "Hello World";
+    }
+}
